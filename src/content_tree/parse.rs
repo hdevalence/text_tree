5db04c322cb7ec1eb17155b8a2d809ef71116fb0
Eliza Weisman <eliza@buoyant.io>
2020-12-26 13:21:18 -0800

    parse open tags with attributes!
    Signed-off-by: Eliza Weisman <eliza@buoyant.io>

--- src/content_tree/parse.rs
+++ src/content_tree/parse.rs
@@ -14,19 +14,49 @@ fn identifier(input: &str) -> IResult<&str, &str> {
     ))(input)
 }
 
+fn attribute_value(input: &str) -> IResult<&str, &str> {
+    preceded(tag("="), delimited(tag("\""), take_until("\""), tag("\"")))(input)
+}
+
+fn named_attribute<'a>(name: &'static str) -> impl FnMut(&'a str) -> IResult<&'a str, &'a str> {
+    preceded(tag(name), attribute_value)
+}
+
+fn any_attribute(input: &str) -> IResult<&str, (&str, &str)> {
+    pair(identifier, attribute_value)(input)
+}
+
 fn open_tag(input: &str) -> IResult<&str, ElementData> {
-    let mut parse_open_tag = delimited(tag("<"), identifier, tag(">"));
-    let (remaining, name) = parse_open_tag(input)?;
+    let (remaining, (tag_name, attrs)) = delimited(
+        tag("<"),
+        pair(
+            identifier,
+            opt(preceded(
+                multispace1,
+                separated_list0(multispace1, any_attribute),
+            )),
+        ),
+        tag(">"),
+    )(input)?;
+
     let mut classes = HashSet::new();
-    classes.insert(String::from(name));
-    Ok((
-        remaining,
-        ElementData {
-            // TODO(eliza)
-            id: None,
-            classes,
-        },
-    ))
+    let mut id = None;
+    for (name, val) in attrs.into_iter().flat_map(IntoIterator::into_iter) {
+        match name {
+            "class" => {
+                classes = val.split(' ').map(String::from).collect();
+            }
+            "id" => {
+                id = Some(val.to_string());
+            }
+            "style" => todo!("style attributes should probably 'work'..."),
+            _ => {
+                // Skip other attributes for now...
+            }
+        }
+    }
+    classes.insert(tag_name.to_string());
+    Ok((remaining, ElementData { id, classes }))
 }
 
 #[cfg(test)]
@@ -35,11 +65,77 @@ mod tests {
     #[test]
     fn simple_open_tag() {
         let open = "<a>";
-        let (remaining, parsed) = dbg!(open_tag(open)).expect("it should parse");
+        let (remaining, parsed) = dbg!(open_tag(open))
+            .map_err(|e| e.to_string())
+            .expect("it should parse");
 
         let mut classes = HashSet::new();
         classes.insert(String::from("a"));
         assert_eq!(parsed, ElementData { classes, id: None });
+        assert_eq!(remaining, "");
+    }
+
+    #[test]
+    fn open_tag_with_ignored_attrs() {
+        let open = "<a href=\"my cool website\">";
+        let (remaining, parsed) = dbg!(open_tag(open))
+            .map_err(|e| e.to_string())
+            .expect("it should parse");
+
+        let mut classes = HashSet::new();
+        classes.insert(String::from("a"));
+        assert_eq!(parsed, ElementData { classes, id: None });
+        assert_eq!(remaining, "");
+    }
+
+    #[test]
+    fn open_tag_with_classes() {
+        let open = "<a class=\"foo bar baz\">";
+        let (remaining, parsed) = dbg!(open_tag(open))
+            .map_err(|e| e.to_string())
+            .expect("it should parse");
+
+        let classes = vec!["a", "foo", "bar", "baz"]
+            .into_iter()
+            .map(String::from)
+            .collect();
+        assert_eq!(parsed, ElementData { classes, id: None });
+        assert_eq!(remaining, "");
+    }
+
+    #[test]
+    fn open_tag_with_classes_and_attrs() {
+        let open = "<a href=\"my website\" class=\"foo bar baz\" something=\"lol\">";
+        let (remaining, parsed) = dbg!(open_tag(open))
+            .map_err(|e| e.to_string())
+            .expect("it should parse");
+
+        let classes = vec!["a", "foo", "bar", "baz"]
+            .into_iter()
+            .map(String::from)
+            .collect();
+        assert_eq!(parsed, ElementData { classes, id: None });
+        assert_eq!(remaining, "");
+    }
+
+    #[test]
+    fn open_tag_with_classes_and_id() {
+        let open = "<a href=\"my website\" class=\"foo bar baz\" id=\"cool\">";
+        let (remaining, parsed) = dbg!(open_tag(open))
+            .map_err(|e| e.to_string())
+            .expect("it should parse");
+
+        let classes = vec!["a", "foo", "bar", "baz"]
+            .into_iter()
+            .map(String::from)
+            .collect();
+        assert_eq!(
+            parsed,
+            ElementData {
+                classes,
+                id: Some("cool".to_string())
+            }
+        );
         assert_eq!(remaining, "");
     }
 }
